1c06bdb5673bfcaa6845164a18e56aff2548ef34
Gabe Labadie <glabadie2001@gmail.com>
2025-03-09 14:25:41 -0700

    Turned PlayerController into a State Machine

---
+++ Assets/Scripts/Helpers.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using System;
+
+public static class Helpers
+{
+    public static void ForEach<T>(this IEnumerable<T> source, Action<T> action)
+    {
+        foreach (T element in source)
+        {
+            action(element);
+        }
+    }
+}
